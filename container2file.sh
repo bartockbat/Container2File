f76d1b9073a0c7186c9f0ffb2439100f32ad4669
bartockbat <gmillard@redhat.com>
2015-08-07 15:29:15 -0400

    Updated script to save it to a file - still kinda crude

--- container2file.sh
+++ container2file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # The $1 parameter in the docker image name
-
-docker history --no-trunc=true $1 | gawk '{print $7,$8,$9,$10,$11,$12,$13,$14,$15,$16}'
+export NOW=$(date +%R"_"%d_%m_%Y)
+docker history --no-trunc=true $1 | gawk '{print $7,$8,$9,$10,$11,$12,$13,$14,$15,$16}' >> file$NOW
 
